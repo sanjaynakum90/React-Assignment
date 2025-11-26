16eac23ded2ec8ff8d2e37450bcc26642753be16
sanjaynakum90 <sanjaynakum4545@gmail.com>
2025-11-26 15:35:49 +0530

    some changes

--- 02-bootstrap/src/Components/Navbar.jsx
+++ 02-bootstrap/src/Components/Navbar.jsx
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import "./../style.css";
 
 export default function Header() {
-    const social = ["x-twitter", "facebook-f", "instagram", "linkedin-in"]
+
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            setScrolled(window.scrollY > 20);
+        };
+
+        window.addEventListener("scroll", onScroll);
+        return () => window.removeEventListener("scroll", onScroll);
+    }, []);
+
+    const social = ["x-twitter", "facebook-f", "instagram", "linkedin-in"];
+
     return (
-        <Navbar expand="xl" fixed="top" className="navbar-dark bg-transparent">
+        <Navbar
+            expand="xl"
+            fixed="top"
+            className={`navbar-dark ${scrolled ? "navbar-flat" : "navbar-rounded"} bg-transparent`}
+        >
             <Container className="nav-container">
+
+                
                 <a className="navbar-brand" href="/">
-                    <img
-                        src="logo-light.png"
-                        alt="Logo"
-                        height="40"
-                        className="d-none d-lg-block"
-                    />
-                    <img
-                        src="logo-sm.png"
-                        alt="Logo"
-                        height="40"
-                        className="d-lg-none"
-                    />
+                    <img src="logo-light.png" alt="Logo" height="40" className="d-none d-lg-block" />
+                    <img src="logo-sm.png" alt="Logo" height="40" className="d-lg-none" />
                 </a>
 
-
+                
                 <div className="d-xl-none d-flex align-items-center social-media gap-2 ms-auto">
                     {social.map((icon, i) => (
                         <a key={i} href="#" className="text-decoration-none">
@@ -33,22 +42,17 @@ export default function Header() {
                     ))}
                 </div>
 
-
                 <Navbar.Toggle aria-controls="navbarNav" className="border-0 p-0 ms-3" />
 
                 <Navbar.Collapse id="navbarNav">
                     <Nav className="mx-auto">
 
-                        <Nav.Link href="/" className="nav-link">
+                        <Nav.Link href="/">
                             <i className="fas fa-home me-1"></i>Home
                         </Nav.Link>
 
                         <Nav.Item className="dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="#about"
-                                data-bs-toggle="dropdown"
-                            >
+                            <a className="nav-link dropdown-toggle" href="#about" data-bs-toggle="dropdown">
                                 <i className="fas fa-info-circle me-1"></i> About
                             </a>
                             <ul className="dropdown-menu dropdown-menu-lg-center">
@@ -73,7 +77,10 @@ export default function Header() {
                         <Nav.Link href="/contact.html">
                             <i className="fas fa-envelope me-1"></i>Contact
                         </Nav.Link>
+
                     </Nav>
+
+                    
                     <div className="d-none d-xl-flex align-items-center social-media gap-3">
                         {social.map((icon, i) => (
                             <a key={i} href="#" className="text-decoration-none">
