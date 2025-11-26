ae84e88631eb35b467279fc2cc11e810c1bf7c1d
sanjaynakum90 <sanjaynakum4545@gmail.com>
2025-11-26 11:32:31 +0530

    some changes

--- 02-bootstrap/src/Components/Navbar.jsx
+++ 02-bootstrap/src/Components/Navbar.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-
+import { Navbar, Nav, Container } from "react-bootstrap";
+import "./../style.css";
 
 export default function Header() {
     return (
-        <nav className="navbar fixed-top navbar-expand-xl">
-            <div className="container nav-container">
-
-                
-                <a className="navbar-brand" href="index.html">
+        <Navbar expand="xl" fixed="top" className="navbar-dark bg-dark">
+            <Container className="nav-container">
+                <a className="navbar-brand" href="/">
                     <img
                         src="logo-light.png"
                         alt="Logo"
@@ -22,46 +21,31 @@ export default function Header() {
                     />
                 </a>
 
-                
-                <div className="d-xl-none d-flex align-items-center justify-content-center social-media gap-2 ms-auto">
-                    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                        <div className="icon-back"><i className="fab fa-x-twitter"></i></div>
-                    </a>
-                    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                        <div className="icon-back"><i className="fab fa-facebook-f"></i></div>
-                    </a>
-                    <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                        <div className="icon-back"><i className="fab fa-instagram"></i></div>
-                    </a>
-                    <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                        <div className="icon-back"><i className="fab fa-linkedin-in"></i></div>
-                    </a>
+
+                <div className="d-xl-none d-flex align-items-center social-media gap-2 ms-auto">
+                    {["x-twitter", "facebook-f", "instagram", "linkedin-in"].map((icon, i) => (
+                        <a key={i} href="#" className="text-decoration-none">
+                            <div className="icon-back">
+                                <i className={`fab fa-${icon}`}></i>
+                            </div>
+                        </a>
+                    ))}
                 </div>
 
-                <button
-                    className="navbar-toggler collapsed border-0 p-0 ms-3"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
 
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav mx-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="index.html" aria-current="page">
-                                <i className="fas fa-home me-1"></i>Home
-                            </a>
-                        </li>
+                <Navbar.Toggle aria-controls="navbarNav" className="border-0 p-0 ms-3" />
 
-                        <li className="nav-item dropdown">
+                <Navbar.Collapse id="navbarNav">
+                    <Nav className="mx-auto">
+
+                        <Nav.Link href="/" className="nav-link">
+                            <i className="fas fa-home me-1"></i>Home
+                        </Nav.Link>
+
+                        <Nav.Item className="dropdown">
                             <a
                                 className="nav-link dropdown-toggle"
-                                href="/"
+                                href="#about"
                                 data-bs-toggle="dropdown"
                             >
                                 <i className="fas fa-info-circle me-1"></i> About
@@ -71,49 +55,36 @@ export default function Header() {
                                 <li><a className="dropdown-item" href="expertise.html">Expertise & Experience</a></li>
                                 <li><a className="dropdown-item" href="clients.html">Client-Centric Approach</a></li>
                             </ul>
-                        </li>
+                        </Nav.Item>
 
-                        <li className="nav-item">
-                            <a className="nav-link" href="/services.html">
-                                <i className="fas fa-cogs me-1"></i>Services
+                        <Nav.Link href="/services.html">
+                            <i className="fas fa-cogs me-1"></i>Services
+                        </Nav.Link>
+
+                        <Nav.Link href="#project">
+                            <i className="fas fa-project-diagram me-1"></i>Projects
+                        </Nav.Link>
+
+                        <Nav.Link href="/blog.html">
+                            <i className="fas fa-blog me-1"></i>Blog
+                        </Nav.Link>
+
+                        <Nav.Link href="/contact.html">
+                            <i className="fas fa-envelope me-1"></i>Contact
+                        </Nav.Link>
+                    </Nav>
+                    <div className="d-none d-xl-flex align-items-center social-media gap-3">
+                        {["x-twitter", "facebook-f", "instagram", "linkedin-in"].map((icon, i) => (
+                            <a key={i} href="#" className="text-decoration-none">
+                                <div className="icon-back">
+                                    <i className={`fab fa-${icon}`}></i>
+                                </div>
                             </a>
-                        </li>
-
-                        <li className="nav-item">
-                            <a className="nav-link" href="#project">
-                                <i className="fas fa-project-diagram me-1"></i>Projects
-                            </a>
-                        </li>
-
-                        <li className="nav-item">
-                            <a className="nav-link" href="/blog.html">
-                                <i className="fas fa-blog me-1"></i>Blog
-                            </a>
-                        </li>
-
-                        <li className="nav-item">
-                            <a className="nav-link" href="/contact.html">
-                                <i className="fas fa-envelope me-1"></i> Contact
-                            </a>
-                        </li>
-                    </ul>
-
-                    <div className="d-none d-xl-flex align-items-center mt-lg-0 mt-4 justify-content-center social-media gap-3">
-                        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                            <div className="icon-back"><i className="fab fa-x-twitter"></i></div>
-                        </a>
-                        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                            <div className="icon-back"><i className="fab fa-facebook-f"></i></div>
-                        </a>
-                        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                            <div className="icon-back"><i className="fab fa-instagram"></i></div>
-                        </a>
-                        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                            <div className="icon-back"><i className="fab fa-linkedin-in"></i></div>
-                        </a>
+                        ))}
                     </div>
-                </div>
-            </div>
-        </nav>
+
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
