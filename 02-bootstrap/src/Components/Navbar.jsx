d54269d96f939bd3e4cf6e4e2114a99df883761c
sanjaynakum90 <sanjaynakum4545@gmail.com>
2025-11-26 15:56:46 +0530

    some changes

--- 02-bootstrap/src/Components/Navbar.jsx
+++ 02-bootstrap/src/Components/Navbar.jsx
@@ -25,13 +25,13 @@ export default function Header() {
         >
             <Container className="nav-container">
 
-                
+
                 <a className="navbar-brand" href="/">
                     <img src="logo-light.png" alt="Logo" height="40" className="d-none d-lg-block" />
                     <img src="logo-sm.png" alt="Logo" height="40" className="d-lg-none" />
                 </a>
 
-                
+
                 <div className="d-xl-none d-flex align-items-center social-media gap-2 ms-auto">
                     {social.map((icon, i) => (
                         <a key={i} href="#" className="text-decoration-none">
@@ -47,9 +47,11 @@ export default function Header() {
                 <Navbar.Collapse id="navbarNav">
                     <Nav className="mx-auto">
 
-                        <Nav.Link href="/">
-                            <i className="fas fa-home me-1"></i>Home
-                        </Nav.Link>
+                        <Nav.Item>
+                            <a href="/" className="nav-link">
+                                <i className=" fas fa-home me-1 "></i>Home
+                            </a>
+                        </Nav.Item>
 
                         <Nav.Item className="dropdown">
                             <a className="nav-link dropdown-toggle" href="#about" data-bs-toggle="dropdown">
@@ -61,26 +63,33 @@ export default function Header() {
                                 <li><a className="dropdown-item" href="clients.html">Client-Centric Approach</a></li>
                             </ul>
                         </Nav.Item>
+                        <Nav.Item>
+                            <a href="/" className="nav-link">
+                                <i className="fas fa-cogs me-1"></i>Services
+                            </a>
+                        </Nav.Item>
 
-                        <Nav.Link href="/services.html">
-                            <i className="fas fa-cogs me-1"></i>Services
-                        </Nav.Link>
+                        <Nav.Item>
+                            <a href="/" className="nav-link">
+                                <i className="fas fa-project-diagram me-1"></i>Projects
+                            </a>
+                        </Nav.Item>
 
-                        <Nav.Link href="#project">
-                            <i className="fas fa-project-diagram me-1"></i>Projects
-                        </Nav.Link>
 
-                        <Nav.Link href="/blog.html">
-                            <i className="fas fa-blog me-1"></i>Blog
-                        </Nav.Link>
-
-                        <Nav.Link href="/contact.html">
-                            <i className="fas fa-envelope me-1"></i>Contact
-                        </Nav.Link>
+                        <Nav.Item>
+                            <a href="/" className="nav-link">
+                                <i className="fas fa-blog me-1"></i>Blog
+                            </a>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <a href="/" className="nav-link">
+                                <i className="fas fa-envelope me-1"></i>Contact
+                            </a>
+                        </Nav.Item>
 
                     </Nav>
 
-                    
+
                     <div className="d-none d-xl-flex align-items-center social-media gap-3">
                         {social.map((icon, i) => (
                             <a key={i} href="#" className="text-decoration-none">
