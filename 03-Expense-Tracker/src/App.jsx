4a3613d77502d104e280aa5d5115d9fa804d8928
sanjaynakum90 <sanjaynakum4545@gmail.com>
2026-01-06 12:10:30 +0530

    update

--- 03-Expense-Tracker/src/App.jsx
+++ 03-Expense-Tracker/src/App.jsx
@@ -3,11 +3,13 @@ import ExpenseForm from './Components/ExpenseForm'
 import ExpenseList from './Components/ExpenseList'
 import ExpenseData from './Components/ExpenseData'
 
+import { GiExpense } from "react-icons/gi";
+
 const App = () => {
   return (
     <>
       <div className="text-center mt-4">
-        <h1>Expense Tarcker</h1>
+        <h1> <GiExpense /> Expense Tarcker</h1>
       </div>
       <ExpenseForm />
       <ExpenseData />
