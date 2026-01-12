faa56aeb2b9b010ea052bb81c75fcb62eff220bb
sanjaynakum90 <sanjaynakum4545@gmail.com>
2026-01-12 10:53:17 +0530

    update

--- 05-Redux-Builder/src/Components/ProductList.jsx
+++ 05-Redux-Builder/src/Components/ProductList.jsx
@@ -5,7 +5,7 @@ import { MdEdit } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
 import { AiOutlineProduct } from "react-icons/ai";
 
-import { Button, Table, Container, Row, Col } from "react-bootstrap"
+import { Button, Table, Container, Row, Col, Card } from "react-bootstrap"
 import { deleteProduct, setUpdateState } from '../features/product/productSlice';
 
 const ProductList = () => {
@@ -13,7 +13,7 @@ const ProductList = () => {
     const Products = useSelector((state) => state.Product.product)
 
 
-    console.log("peo",Products)
+    console.log("peo", Products)
 
     const Dispatch = useDispatch()
 
@@ -26,49 +26,62 @@ const ProductList = () => {
             <Container>
                 <Row>
                     <Col>
-                        <Table>
-                            <thead>
-                                <tr className='table-dark'>
-                                    <td>Id</td>
-                                    <td>Name</td>
-                                    <td>Price</td>
-                                    <td>Quantity</td>
-                                    <td>Category</td>
-                                    <td>Total price</td>
-                                    <td colSpan={2}>Actions</td>
-                                </tr>
-                            </thead>
+                        {Products.length == 0 ?
+                            (
+                                <Container >
+                                    <Card className='my-3 p-3 shadow'>
+                                        <div className="text-center"><FaBoxOpen fontSize={50} /></div>
+                                        <p className='text-center fs-4'>No Data Found</p>
+                                        <span className='text-center'>Please Enter the Product Data.....</span>
+                                    </Card>
 
-                            <tbody>
-                                {Products.map((prod, index) => (
-                                    <tr key={prod.id}>
-                                        <td>{index + 1}</td>
-                                        <td>{prod.Name}</td>
-                                        <td>{prod.Price}</td>
-                                        <td>{prod.Quantity}</td>
-                                        <td>{prod.Category}</td>
-                                        <td>{prod.Quantity * prod.Price}</td>
-                                        <td>
-                                            {
-                                                <Button variant='outline-warning' onClick={() => Dispatch(setUpdateState(prod))}>
-                                                    <MdEdit fontSize={18} />
-                                                </Button>
-                                            }
-                                        </td>
 
-                                        <td>
-                                            {
+                                </Container>
+                            ) : (
+                                <Table striped bordered hover shadow>
+                                    <thead>
+                                        <tr className='table-dark'>
+                                            <td>Id</td>
+                                            <td>Name</td>
+                                            <td>Price</td>
+                                            <td>Quantity</td>
+                                            <td>Category</td>
+                                            <td>Total price</td>
+                                            <td colSpan={2}>Actions</td>
+                                        </tr>
+                                    </thead>
 
-                                                <Button variant='outline-danger' onClick={() => Dispatch(deleteProduct(prod.id))}>
-                                                    <MdDelete fontSize={18} />
-                                                </Button>
-                                            }
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
+                                    <tbody>
+                                        {Products.map((prod, index) => (
+                                            <tr key={prod.id}>
+                                                <td>{index + 1}</td>
+                                                <td>{prod.Name}</td>
+                                                <td>{prod.Price}</td>
+                                                <td>{prod.Quantity}</td>
+                                                <td>{prod.Category}</td>
+                                                <td>{prod.Quantity * prod.Price}</td>
+                                                <td>
+                                                    {
+                                                        <Button variant='outline-warning' onClick={() => Dispatch(setUpdateState(prod))}>
+                                                            <MdEdit fontSize={18} />
+                                                        </Button>
+                                                    }
+                                                </td>
 
-                        </Table >
+                                                <td>
+                                                    {
+
+                                                        <Button variant='outline-danger' onClick={() => Dispatch(deleteProduct(prod.id))}>
+                                                            <MdDelete fontSize={18} />
+                                                        </Button>
+                                                    }
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+
+                                </Table >
+                            )}
                     </Col>
                 </Row>
             </Container>
